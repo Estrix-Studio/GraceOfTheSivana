968aad294bb199f3a76a25c4d015c49739ca864b
Andrii Gastello <concof.zp@gmail.com>
2023-09-12 17:21:26 -0400

    Adding GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("<color=green><b>Hello, World!</b></color>");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
