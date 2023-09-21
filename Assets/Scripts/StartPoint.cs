294d71e55bb2b8674cfbf9f1783a1f37d5950d31
Stanislav Voronov <stasv2003@gmail.com>
2023-09-21 19:17:20 -0400

    added bg change on scene transfer

--- Assets/Scripts/StartPoint.cs
+++ Assets/Scripts/StartPoint.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class StartPoint : MonoBehaviour
 {
+    [SerializeField] private Color backgroundColor;
     private void Start()
     {
         StaticPlayer.Instance.transform.position = transform.position;
+        StaticPlayer.Instance.GetComponentInChildren<Camera>().backgroundColor = backgroundColor;
     }
 }
