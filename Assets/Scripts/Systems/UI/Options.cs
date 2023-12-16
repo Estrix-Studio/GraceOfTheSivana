e37de2bd56e1af1d83c7536086e382ab33359a6e
Andrii Gastello <concof.zp@gmail.com>
2023-12-16 18:07:07 -0500

    Finished Options Tab in MainMenu/UI
    - Added Sliders for Music and SFX Volume
    - Now sliders write info into PlayerPrefs

--- Assets/Scripts/Systems/UI/Options.cs
+++ Assets/Scripts/Systems/UI/Options.cs
@@ -1,4 +1,3 @@
-using System;
 using Adventure;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,26 +8,66 @@ namespace Systems.UI
     public class Options : MonoBehaviour
     {
         [SerializeField] private Button backButton;
-        
-        // Start is called before the first frame update
-        void Start()
-        {
 
+        [SerializeField] private Slider sfxVolumeSlider;
+        [SerializeField] private Slider musicVolumeSlider;
+
+        private void Start()
+        {
+            // Load saved volume settings
+            LoadVolumeSettings();
         }
 
         private void OnEnable()
         {
             backButton.onClick.AddListener(GoBack);
+            // Add listeners for volume sliders
+            sfxVolumeSlider.onValueChanged.AddListener(SetSfxVolume);
+            musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
         }
 
         private void OnDisable()
         {
             backButton.onClick.RemoveListener(GoBack);
+            // Remove listeners for volume sliders
+            sfxVolumeSlider.onValueChanged.RemoveListener(SetSfxVolume);
+            musicVolumeSlider.onValueChanged.RemoveListener(SetMusicVolume);
         }
 
         private static void GoBack()
         {
+            // Save volume settings before going back to MainMenu
+            SaveVolumeSettings();
             SceneManager.LoadScene("MainMenu");
+        }
+
+        public void SetSfxVolume(float volume)
+        {
+            // Implement setting SFX volume logic here
+            // You may use AudioManager or other methods to handle audio volume
+            PlayerPrefs.SetFloat("SfxVolume", volume);
+            Debug.Log("<color=blue>SFX Volume: " + PlayerPrefs.GetFloat("SfxVolume") + "</color>");
+        }
+
+        public void SetMusicVolume(float volume)
+        {
+            // Implement setting MUSIC volume logic here
+            // You may use AudioManager or other methods to handle audio volume
+            PlayerPrefs.SetFloat("MusicVolume", volume);
+            Debug.Log("<color=blue>Music Volume: " + PlayerPrefs.GetFloat("MusicVolume") + "</color>");
+        }
+
+        private void LoadVolumeSettings()
+        {
+            // Load saved volume settings or use default values
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat("SfxVolume", 0.5f);
+            musicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        }
+
+        private static void SaveVolumeSettings()
+        {
+            // Save volume settings
+            PlayerPrefs.Save();
         }
     }
 }
